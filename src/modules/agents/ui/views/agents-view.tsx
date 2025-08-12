256a78483ece1d898490bf36a090b126d29f532d
jasilmohmd <jasilcheruppa@gmail.com>
2025-08-12 18:29:51 +0530

    agent page implemented

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useRouter } from "next/navigation";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { useTRPC } from "@/trpc/client";
@@ -17,22 +18,25 @@ import { DataPagination } from "../components/data-pagination";
 
 export const AgentsView = () => {
 
-  const [ filters, setFilters ] = useAgentsFilters();
-  console.log(filters.search);
+  const router = useRouter();
+  const [filters, setFilters] = useAgentsFilters();
+
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
     ...filters,
   }));
 
-  console.log(data);
-
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable data={data.items} columns={columns} />
+      <DataTable
+        data={data.items}
+        columns={columns}
+        onRowClick={(row)=> router.push(`/agents/${row.id}`)}
+      />
       <DataPagination
         page={filters.page}
         totalPages={data.totalPages}
-        onPageChange={(page) => setFilters({page})}
+        onPageChange={(page) => setFilters({ page })}
       />
       {data.items.length === 0 && (
         <EmptyState
