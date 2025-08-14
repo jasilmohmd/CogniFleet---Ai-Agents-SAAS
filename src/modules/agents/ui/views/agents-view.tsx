07bc557662a0db7b0a0076352f6acdeb83e4c625
jasilmohmd <jasilcheruppa@gmail.com>
2025-08-14 12:47:22 +0530

    meetings data-table implemented

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -5,12 +5,12 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { useTRPC } from "@/trpc/client";
 
+import { DataTable } from "@/components/data-table";
 import { ErrorState } from "@/components/error-state";
 import { EmptyState } from "@/components/empty-state";
 import { LoadingState } from "@/components/loading-state";
 
 import { columns } from "../components/columns";
-import { DataTable } from "../components/data-table";
 
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { DataPagination } from "../components/data-pagination";
